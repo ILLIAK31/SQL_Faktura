243e9f77cc8c8f8988bce0dd719cb0f8346f1e8a
Illia <karma@MYPC>
2024-01-04 19:23:06 +0100

    Add project files.

---
+++ App10.2/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace App10._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new System.Windows.Forms.Button();
+            button_delete = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            textBox1 = new System.Windows.Forms.TextBox();
+            textBox2 = new System.Windows.Forms.TextBox();
+            dataGridView1 = new System.Windows.Forms.DataGridView();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            textBox3 = new System.Windows.Forms.TextBox();
+            button_update = new System.Windows.Forms.Button();
+            button_Sort = new System.Windows.Forms.Button();
+            button_pos = new System.Windows.Forms.Button();
+            textBox4 = new System.Windows.Forms.TextBox();
+            label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(364, 25);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(190, 58);
+            button1.TabIndex = 0;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button_delete
+            // 
+            button_delete.Location = new System.Drawing.Point(582, 159);
+            button_delete.Name = "button_delete";
+            button_delete.Size = new System.Drawing.Size(171, 58);
+            button_delete.TabIndex = 1;
+            button_delete.Text = "Delete";
+            button_delete.UseVisualStyleBackColor = true;
+            button_delete.Click += button_delete_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(788, 25);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(180, 58);
+            button3.TabIndex = 2;
+            button3.Text = "Read";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(18, 109);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(278, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(18, 261);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(278, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new System.Drawing.Point(364, 261);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new System.Drawing.Size(619, 306);
+            dataGridView1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(69, 25);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(175, 43);
+            label1.TabIndex = 7;
+            label1.Text = "Number";
+            // 
+            // label2
+            // 
+            label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label2.Location = new System.Drawing.Point(69, 174);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(175, 43);
+            label2.TabIndex = 8;
+            label2.Text = "Value";
+            // 
+            // label3
+            // 
+            label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label3.Location = new System.Drawing.Point(640, 24);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(113, 43);
+            label3.TabIndex = 9;
+            label3.Text = "ID";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new System.Drawing.Point(582, 99);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(171, 27);
+            textBox3.TabIndex = 10;
+            // 
+            // button_update
+            // 
+            button_update.Location = new System.Drawing.Point(364, 159);
+            button_update.Name = "button_update";
+            button_update.Size = new System.Drawing.Size(190, 58);
+            button_update.TabIndex = 11;
+            button_update.Text = "Update";
+            button_update.UseVisualStyleBackColor = true;
+            button_update.Click += button_update_Click;
+            // 
+            // button_Sort
+            // 
+            button_Sort.Location = new System.Drawing.Point(788, 159);
+            button_Sort.Name = "button_Sort";
+            button_Sort.Size = new System.Drawing.Size(180, 58);
+            button_Sort.TabIndex = 12;
+            button_Sort.Text = "Sort";
+            button_Sort.UseVisualStyleBackColor = true;
+            button_Sort.Click += button_Sort_Click;
+            // 
+            // button_pos
+            // 
+            button_pos.Location = new System.Drawing.Point(54, 481);
+            button_pos.Name = "button_pos";
+            button_pos.Size = new System.Drawing.Size(190, 58);
+            button_pos.TabIndex = 13;
+            button_pos.Text = "Add Position";
+            button_pos.UseVisualStyleBackColor = true;
+            button_pos.Click += button_pos_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new System.Drawing.Point(18, 405);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new System.Drawing.Size(278, 27);
+            textBox4.TabIndex = 14;
+            // 
+            // label4
+            // 
+            label4.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label4.Location = new System.Drawing.Point(69, 329);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(175, 43);
+            label4.TabIndex = 15;
+            label4.Text = "Name";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1009, 591);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(button_pos);
+            Controls.Add(button_Sort);
+            Controls.Add(button_update);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button_delete);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_delete;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button_update;
+        private System.Windows.Forms.Button button_Sort;
+        private System.Windows.Forms.Button button_pos;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ App10.2/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.Extensions.Options;
+using System.Xml.Linq;
+
+namespace App10._2
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlTransaction transaction; //
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(@"Data Source=MYPC\SQLEXPRESS01;Initial Catalog=Lab10;Trusted_Connection=True;Integrated Security=True;MultipleActiveResultSets=True;TrustServercertificate=True");
+            cmd = new SqlCommand();
+            cmd.Connection = conn;
+            displaydata();
+        }
+        private void ExecuteNonQueryWithTransaction(string query)
+        {
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+        }
+        private void displaydata()
+        {
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from invoice";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+        private void cleardata()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "insert into invoice (number,value) values(@Value1,@Value2)";
+            cmd.CommandText = query;
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@Value1", textBox1.Text);
+            cmd.Parameters.AddWithValue("@Value2", "0");
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            cleardata();
+            conn.Close();
+            displaydata();
+        }
+
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            //string query = "delete invoice where invoice_id=@Value3";
+            //cmd.CommandText = query;
+            //cmd.Parameters.Clear();
+            //cmd.Parameters.AddWithValue("@Value3", textBox3.Text.ToString());
+            //conn.Open();
+            //cmd.ExecuteNonQuery();
+            //dataGridView1.DataSource = query;
+            //cleardata();
+            //conn.Close();
+            //displaydata();
+            int invoiceId = Convert.ToInt32(textBox3.Text);
+
+            // Delete records from invoice_pos table based on invoice_id
+            string posQuery = "DELETE FROM invoice_pos WHERE invoice_id = @Value3";
+            cmd.CommandText = posQuery;
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@Value3", invoiceId);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            // Delete record from invoice table based on invoice_id
+            string invoiceQuery = "DELETE FROM invoice WHERE invoice_id = @Value3";
+            cmd.CommandText = invoiceQuery;
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@Value3", invoiceId);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            cleardata();
+            displaydata();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            displaydata();
+        }
+
+        private void button_update_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update invoice set number='" + textBox1.Text + "',value='" + textBox2.Text.ToString() + "' where invoice_id='" + textBox3.Text.ToString() + "' ";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            displaydata();
+            cleardata();
+        }
+        private bool isSortAscending = true;
+        private void button_Sort_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            string sortOrder = isSortAscending ? "ASC" : "DESC";
+            cmd.CommandText = $"select * from invoice order by value {sortOrder}";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt.DefaultView;
+            isSortAscending = !isSortAscending;
+            conn.Close();
+        }
+        private void DisplaySumForInvoiceId(int invoiceId)
+        {
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+
+            // Calculate the sum of values for the specified invoice_id
+            cmd.CommandText = $"SELECT SUM(value) AS TotalSum FROM invoice_pos WHERE invoice_id = {invoiceId}";
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                if (!reader.IsDBNull(reader.GetOrdinal("TotalSum")))
+                {
+                    decimal totalSum = reader.GetDecimal(reader.GetOrdinal("TotalSum"));
+
+                    // Close the existing DataReader
+                    reader.Close();
+
+                    // Update the value column in the invoice table
+                    cmd.CommandText = $"UPDATE invoice SET value = {totalSum} WHERE invoice_id = {invoiceId}";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            conn.Close();
+        }
+        private void button_pos_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox3.Text, out int invoiceId))
+            {
+                string query = "insert into invoice_pos (invoice_id,name,value) values(@Value1,@Value2,@Value3)";
+                cmd.CommandText = query;
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@Value1", textBox3.Text);
+                cmd.Parameters.AddWithValue("@Value2", textBox4.Text);
+                cmd.Parameters.AddWithValue("@Value3", textBox2.Text.ToString());
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                cleardata();
+                conn.Close();
+                //int invoiceId = Convert.ToInt32(textBox3.Text);
+                DisplaySumForInvoiceId(invoiceId);
+                displaydata();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid integer for Invoice ID.");
+            }
+        }
+    }
+}
